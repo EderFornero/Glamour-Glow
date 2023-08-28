6b94ae1c59bcf7cb627c13ffc8ae9555357e071a
Eder <ederfornero.ef@gmail.com>
2023-08-28 19:53:17 -0300

    build: :hammer: base-structure-created
    Built a body files.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-const App = () => {
+const App = (): JSX.Element => {
   const [count, setCount] = useState(0)
 
   return (
