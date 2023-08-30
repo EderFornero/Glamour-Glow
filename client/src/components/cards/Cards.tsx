425acd9c40f7b1616a721570d981c71cc2555ecb
Lucas Cubile <cubilelucas@gmail.com>
2023-08-30 15:29:02 -0300

    fix: :art: switched-to-css-modules-to-prevent-styles-overlap

--- client/src/components/cards/Cards.tsx
+++ client/src/components/cards/Cards.tsx
@@ -1,11 +1,11 @@
 import { users } from '../../../../mocks/fullAPIresponse.json'
 import ServiceCard from '../ServiceCard/ServiceCard'
 import type { ServiceProvider } from '../../interfaces'
-import './Cards.css'
+import style from './Cards.module.css'
 
 const Cards = (): JSX.Element => {
   return (
-    <section className='cardsSection'>
+    <section className={style.cardsSection}>
       {/* Loader while services are loading */}
       {users.map(({ id, businessName, businessLocation, rating, categories, services }: ServiceProvider) => {
         return <ServiceCard key={id} id={id} businessName={businessName} businessLocation={businessLocation} rating={rating} categories={categories} services={services} />
