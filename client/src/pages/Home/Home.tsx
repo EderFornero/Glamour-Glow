4318d160a0df22d25b89c3daafdef1ee8e6a218a
Lucas Cubile <cubilelucas@gmail.com>
2023-08-29 22:17:36 -0300

    style: :art: card-style

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -1,9 +1,11 @@
+import Cards from '../../components/Cards/Cards'
 import Nav from '../../components/Nav/Nav'
 
 const Home: React.FC = () => {
   return (
     <div>
       <Nav />
+      <Cards />
     </div>
   )
 }
