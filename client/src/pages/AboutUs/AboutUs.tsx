a2e2431b96044e36c33f579873b56bb40a1ca36d
Eder <ederfornero.ef@gmail.com>
2023-08-30 14:52:42 -0300

    fix: :bug: completed-about-styles

--- client/src/pages/AboutUs/AboutUs.tsx
+++ client/src/pages/AboutUs/AboutUs.tsx
@@ -15,16 +15,16 @@ const AboutUs = (): JSX.Element => {
       <div className='card-container'>
         {aboutUsData.map(us => (
           <>
-            <div className="card" key={us.id}>
-              <div className="card-info">
+            <div className="card-about" key={us.id}>
+              <div className="card-about-info">
                 <div><img src={us.image} className="card-avatar"/></div>
-                <div className="card-title">{us.name}</div>
-                <div className="card-subtitle">FullStack Developer</div>
+                <div className="card-about-title">{us.name}</div>
+                <div className="card-about-subtitle">FullStack Developer</div>
               </div>
-              <div className='card-description'>
+              <div className='card-about-description'>
                 <p>{us.description}</p>
               </div>
-              <div className="card-social">
+              <div className="card-about-social">
                   <a href={us.linkedin} target='_blank' rel='noreferrer'><li className='card-social__item'><LinkedInSvg /></li></a>
                   <a href={us.github} target='_blank' rel='noreferrer'><li className='card-social__item'><GitHubSvg /></li></a>
               </div>
