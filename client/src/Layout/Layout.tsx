bd35ed7b39fc4d28bdb162c306882b1888d1047d
Lucas Cubile <cubilelucas@gmail.com>
2023-08-30 15:33:07 -0300

    fix: :art: switched-about-to-css-modules

--- client/src/Layout/Layout.tsx
+++ client/src/Layout/Layout.tsx
@@ -2,9 +2,9 @@ import { Outlet } from 'react-router-dom'
 
 const Layout: React.FC = () => {
   return (
-      <>
-        <Outlet />
-      </>
+    <>
+      <Outlet />
+    </>
   )
 }
 
--- client/src/pages/AboutUs/AboutUs.tsx
+++ client/src/pages/AboutUs/AboutUs.tsx
@@ -1,35 +1,43 @@
-// hooks
 import { useAboutUsData } from '../../hooks/index'
-// css
-import './AboutUs.css'
-// svg
+import styles from './AboutUs.module.css'
+
 import GitHubSvg from '../../assets/AboutSvg/GitHub'
 import LinkedInSvg from '../../assets/AboutSvg/LinkedInSvg'
+
 const AboutUs = (): JSX.Element => {
   const aboutUsData = useAboutUsData()
+
   return (
-    <div className='div-container-about'>
-      <div className='get-to-know-us'>
+    <div className={styles['div-container-about']}>
+      <div className={styles['get-to-know-us']}>
         <h2>Get to know us!</h2>
       </div>
-      <div className='card-container'>
-        {aboutUsData.map(us => (
-          <>
-            <div className="card-about" key={us.id}>
-              <div className="card-about-info">
-                <div><img src={us.image} className="card-avatar"/></div>
-                <div className="card-about-title">{us.name}</div>
-                <div className="card-about-subtitle">FullStack Developer</div>
+      <div className={styles['card-container']}>
+        {aboutUsData.map((us) => (
+          <div className={styles['card-about']} key={us.id}>
+            <div className={styles['card-about-info']}>
+              <div>
+                <img src={us.image} className={styles['card-avatar']} />
               </div>
-              <div className='card-about-description'>
-                <p>{us.description}</p>
-              </div>
-              <div className="card-about-social">
-                  <a href={us.linkedin} target='_blank' rel='noreferrer'><li className='card-social__item'><LinkedInSvg /></li></a>
-                  <a href={us.github} target='_blank' rel='noreferrer'><li className='card-social__item'><GitHubSvg /></li></a>
-              </div>
+              <div className={styles['card-about-title']}>{us.name}</div>
+              <div className={styles['card-about-subtitle']}>FullStack Developer</div>
             </div>
-          </>
+            <div className={styles['card-about-description']}>
+              <p>{us.description}</p>
+            </div>
+            <div className={styles['card-about-social']}>
+              <a href={us.linkedin} target='_blank' rel='noreferrer'>
+                <li className={styles['card-social__item']}>
+                  <LinkedInSvg />
+                </li>
+              </a>
+              <a href={us.github} target='_blank' rel='noreferrer'>
+                <li className={styles['card-social__item']}>
+                  <GitHubSvg />
+                </li>
+              </a>
+            </div>
+          </div>
         ))}
       </div>
     </div>
