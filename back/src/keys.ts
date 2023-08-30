520fa552db63e052ac5c74fe4d313b04b309a695
Sebaa <sebaamdq96@gmail.com>
2023-08-29 22:28:13 -0300

    build: :construction_worker: env-data-base

--- back/src/keys.ts
+++ back/src/keys.ts
@@ -1,5 +1,5 @@
-require('dotenv').config()
+import  "dotenv/config"
 const {LOCAL_HOST, DB_NAME} = process.env;
 export const mongodb = {
-    "URI" : `mongodb://${LOCAL_HOST}/${DB_NAME}`
+    URI : `mongodb://${LOCAL_HOST}/${DB_NAME}`
 }
