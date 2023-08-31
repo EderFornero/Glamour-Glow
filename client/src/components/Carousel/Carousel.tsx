5b8417dc763933b39e7ed58bb8d133f4da3e22c9
Eder <ederfornero.ef@gmail.com>
2023-08-31 10:18:16 -0300

    chore: :hammer: carousel-sintaxis

--- client/src/components/Carousel/Carousel.tsx
+++ client/src/components/Carousel/Carousel.tsx
@@ -1,33 +1,33 @@
 import type { ServiceProvider } from '../../interfaces'
 import { NavLink } from 'react-router-dom'
-import { Swiper,SwiperSlide } from 'swiper/react';
-import styles from './Carousel.module.css';
-import 'swiper/css';
+import { Swiper, SwiperSlide } from 'swiper/react'
+import styles from './Carousel.module.css'
+import 'swiper/css'
 
 interface CarouselProps {
-    users: ServiceProvider[]; 
-  }
+  users: ServiceProvider[]
+}
 
-const Carousel:React.FC<CarouselProps> = ({users}) => {
+const Carousel: React.FC<CarouselProps> = ({ users }) => {
   return (
     <Swiper className={styles.swiper} spaceBetween={50} slidesPerView={3} loop navigation pagination autoplay>
         {users.map(({ id, businessName, rating, categories }: ServiceProvider) => {
-        return <SwiperSlide className={styles.swiperslide}>
-            <article className={styles.cards}>
+          return <SwiperSlide className={ styles.swiperslide } key={id}>
+            <article className={ styles.cards }>
                 {/* <img src="source" alt="alt" /> */}
                 <h2>Name: {businessName}</h2>
                 <p>Rating: {rating}</p>
                 {categories.map((category) => {
-                    return <p key={id}>{category}</p>
+                  return <p key={id}>{category}</p>
                 })}
                 <NavLink to='ROUTE HERE'>
                     <button>Check it out</button>
                 </NavLink>
             </article>
-        </SwiperSlide> 
-    })}   
+        </SwiperSlide>
+        })}
     </Swiper>
-  );
-};
+  )
+}
 
 export default Carousel
