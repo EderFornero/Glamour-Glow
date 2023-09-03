ee8977b4322f6c01bf16cd7f05926b22b70da255
GCespedes <giovannice22@hotmail.com>
2023-09-03 14:10:53 -0500

    build: :hammer: carrousel-pagination-add

--- client/src/components/Carousel/Carousel.tsx
+++ client/src/components/Carousel/Carousel.tsx
@@ -1,8 +1,12 @@
 import type { ServiceProvider } from '../../interfaces';
 import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination, A11y, Autoplay } from 'swiper/modules'
 import styles from './Carousel.module.css';
 import 'swiper/css';
 import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+import 'swiper/css/autoplay'
+
 import BusinessCard from '../BusinessCard/BusinessCard';
 import { useEffect, useState } from 'react';
 
@@ -34,13 +38,12 @@ const Carousel: React.FC<CarouselProps> = ({ cardstoshow, carouselName }) => {
     <section className={styles.carousel}>
       <h3 className={styles.title}>{carouselName}</h3>
       <Swiper
+        modules={[Pagination, A11y, Autoplay]}
         className={styles.swiper}
         spaceBetween={160}
         slidesPerView={slidesPerView}
-        loop
-        navigation
         pagination={{ clickable: true }}
-        autoplay
+        autoplay={{ delay: 3000 }}
       >
         {cardstoshow.map(
           ({ id, businessName, rating, categories }: ServiceProvider) => {
