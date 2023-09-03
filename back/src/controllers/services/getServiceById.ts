bf4de4ae2e9324403cfd99efe4e393a8dd991935
Sebaa <sebaamdq96@gmail.com>
2023-09-02 21:06:08 -0300

    build: :hammer: change-controller-and-handler-service
    modifications to drivers and functions linked to services

--- back/src/controllers/services/getServiceById.ts
+++ back/src/controllers/services/getServiceById.ts
@@ -1,13 +1,17 @@
-import { readServiceById } from "../../handlers/index.ts"
-import { Request,Response } from "express"
-import { updateServiceTypeParams } from "../../schemas/serviceSchema.ts"
+import { readServiceById } from "../../handlers/index.ts";
+import { NextFunction, Request, Response } from "express";
+import { updateServiceTypeParams } from "../../schemas/serviceSchema.ts";
 
-export const getServiceById = async(req: Request<updateServiceTypeParams>,res : Response) => {
-    try {
-        const {id} = req.params
-        const serviceById = await readServiceById(id)
-        return res.status(200).json(serviceById)
-    } catch (error) {
-        return error
-    }
-}
+export const getServiceById = async (
+  req: Request<updateServiceTypeParams>,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const id  = req.params.id;
+    const serviceById = await readServiceById(id);
+    return res.status(200).json(serviceById);
+  } catch (error) {
+    return next(error);
+  }
+};
