0d6cb152e790ccd3ad86946576355475ac0d4d6e
lolapernigotti_ <pernigottilola@gmail.com>
2023-09-05 20:55:39 -0300

    build: :construction: remove-middleware-login
    removing the middleware auth from /login route

--- back/src/routes/users/users.ts
+++ back/src/routes/users/users.ts
@@ -22,7 +22,7 @@ const router = Router();
 router.get("/",passport.authenticate('jwt',{session: false}),getUser);
 router.get("/:id", passport.authenticate('jwt',{session: false}),schemaValidation(readAndDeleteUserSchema), getUserByid);
 router.post("/", schemaValidation(CreateUserSchema), postUser);
-router.post("/login",passport.authenticate('jwt',{session: false}), logInUser);
+router.post("/login", logInUser);
 router.put("/:id", passport.authenticate('jwt',{session: false}), schemaValidation(updateUserSchema), putUser);
 router.delete("/:id",passport.authenticate('jwt',{session: false}), schemaValidation(readAndDeleteUserSchema), deleteUser);
 
