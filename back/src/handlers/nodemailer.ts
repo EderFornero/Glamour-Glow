0319c94256aeb69611c8d42e62ce0c9c80e7b329
Eder <ederfornero.ef@gmail.com>
2023-09-06 19:01:10 -0300

    build: :construction: nodemailer-throw-new-error

--- back/src/handlers/nodemailer.ts
+++ back/src/handlers/nodemailer.ts
@@ -2,6 +2,6 @@ import { UserModel } from "../models"
 
 export const findUserByEmailHandler = async (recipientEmail: string) => {
     const userEmail = await UserModel.findOne({email: recipientEmail})
-    if(!userEmail){throw new Error("The email does not wxist")}
+    if(!userEmail){throw new Error("No user was found under the provided E-mail address")}
     return userEmail;
 }
