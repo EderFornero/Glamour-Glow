41b3c7dc99c6e26a59db7e1b49f5ab20391bbf47
Eder <ederfornero.ef@gmail.com>
2023-09-07 13:01:19 -0300

    build: :construction_worker: templates-html-welcome-and-password-reset

--- back/src/handlers/nodemailer.ts
+++ back/src/handlers/nodemailer.ts
@@ -1,7 +1,7 @@
 import { UserModel } from "../models"
 
 export const findUserByEmailHandler = async (recipientEmail: string) => {
-    const userEmail = await UserModel.findOne({email: recipientEmail})
+    const userEmail = await UserModel.findOne({email: recipientEmail}, 'email')
     if(!userEmail){throw new Error("No user was found under the provided E-mail address")}
     return userEmail;
 }
