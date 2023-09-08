201635bd11d862ce0dc24570a2fcfbe49748f8cb
RicardoCS18 <ricardog.carreras@gmail.com>
2023-09-07 22:39:01 -0500

    build: :rotating_light: login-third-party
    fixed camelCase to log in with google

--- back/src/controllers/users/loginUserGoogle.ts
+++ back/src/controllers/users/loginUserGoogle.ts
@@ -6,21 +6,28 @@ export const logInUserGoogle = async (req: Request, res: Response) => {
   try {
     const { email, password, image, name } = req.body;
 
-
-    const user = await UserModel.find({ email })
+    const user = await UserModel.find({ email });
     if (user.length < 1) {
-      console.log('entre en el if')
-      const newUser = await UserModel.create({ email: email, password: password, image: image, name: name, date_of_birth: '2000-01-01', phone_number: '999999999', last_name: 'apellido', role: 'customer'});
+      console.log("entre en el if");
+      const newUser = await UserModel.create({
+        email: email,
+        password: password,
+        image: image,
+        name: name,
+        dateOfBirth: "2000-01-01",
+        phoneNumber: "999999999",
+        lastName: "apellido",
+        role: "customer",
+      });
       console.log(newUser);
-      const token = await generateToken(email)
-      console.log(token)
-      return res.status(200).send(token)
+      const token = await generateToken(email);
+      console.log(token);
+      return res.status(200).send(token);
     }
 
-    const token = await generateToken(email)
-    console.log(token)
-    return res.status(200).send(token)
-
+    const token = await generateToken(email);
+    console.log(token);
+    return res.status(200).send(token);
   } catch (error: any) {
     return res.status(400).send({
       message: "Failed authentication, incorrect credentials",
--- back/src/routes/users/users.ts
+++ back/src/routes/users/users.ts
@@ -1,18 +1,48 @@
-import { Router } from 'express'
-import { schemaValidation } from '../../middlewares/schemaValidator.middleware'
-import { CreateUserSchema, readAndDeleteUserSchema, updateUserSchema } from '../../schemas/userSchema'
-import { deleteUser, getUser, postUser, getUserByid, putUser, logInUser } from '../../controllers/users/index'
-import { rolePermissions } from '../../middlewares/rolePermissions.middleware'
-import passport from 'passport'
+import { Router } from "express";
+import { schemaValidation } from "../../middlewares/schemaValidator.middleware";
+import {
+  CreateUserSchema,
+  readAndDeleteUserSchema,
+  updateUserSchema,
+} from "../../schemas/userSchema";
+import {
+  deleteUser,
+  getUser,
+  postUser,
+  getUserByid,
+  putUser,
+  logInUser,
+  logInUserGoogle,
+} from "../../controllers/users/index";
+import { rolePermissions } from "../../middlewares/rolePermissions.middleware";
+import passport from "passport";
 
-const router = Router()
+const router = Router();
 
-router.get('/', passport.authenticate('jwt', { session: false }), getUser)
-router.get('/:id', passport.authenticate('jwt', { session: false }), rolePermissions('customer'), schemaValidation(readAndDeleteUserSchema), getUserByid)
-router.post('/', schemaValidation(CreateUserSchema), postUser)
-router.post('/login', logInUser)
+router.get("/", passport.authenticate("jwt", { session: false }), getUser);
+router.get(
+  "/:id",
+  passport.authenticate("jwt", { session: false }),
+  rolePermissions("customer"),
+  schemaValidation(readAndDeleteUserSchema),
+  getUserByid
+);
+router.post("/", schemaValidation(CreateUserSchema), postUser);
+router.post("/login", logInUser);
+router.post("/auth/login", logInUserGoogle);
+router.put(
+  "/:id",
+  passport.authenticate("jwt", { session: false }),
+  rolePermissions("customer"),
+  schemaValidation(updateUserSchema),
+  putUser
+);
+router.delete(
+  "/:id",
+  passport.authenticate("jwt", { session: false }),
+  rolePermissions("customer"),
+  schemaValidation(readAndDeleteUserSchema),
+  deleteUser
+);
 
-router.put('/:id', passport.authenticate('jwt', { session: false }), rolePermissions('customer'), schemaValidation(updateUserSchema), putUser)
-router.delete('/:id', passport.authenticate('jwt', { session: false }), rolePermissions('customer'), schemaValidation(readAndDeleteUserSchema), deleteUser)
-
-export default router
+export default router;
