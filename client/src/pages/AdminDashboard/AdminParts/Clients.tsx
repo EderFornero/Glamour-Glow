b0cb4a9614df904b91ec7a6a25c9b3e6a465758a
Lucas Cubile <cubilelucas@gmail.com>
2023-09-12 11:44:15 -0300

    build: :building_construction: fix-types

--- client/src/pages/AdminDashboard/AdminParts/Clients.tsx
+++ client/src/pages/AdminDashboard/AdminParts/Clients.tsx
@@ -23,32 +23,36 @@ const Clients: React.FC<Props> = ({ services }) => {
   }
 
   return (
-    <div className= {style['seller-services-container']}>
-      <div className= {style['seller-services-header']}>
+    <div className={style['seller-services-container']}>
+      <div className={style['seller-services-header']}>
         <h2>Clients</h2>
       </div>
       <ul className={style['service-list']}>
         {services.map((service, index) => (
           <li key={service.id} className={style['service-item']}>
-            {editedServiceId === service.id
-              ? (<>
+            {editedServiceId === service.id ? (
+              <>
                 <input
-                  type="text"
+                  type='text'
                   value={service.name}
                   onChange={(e) => {
                     console.log(`Editar servicio con ID ${service.id}: ${e.target.value}`)
                   }}
                 />
                 <button onClick={() => setEditedServiceId(null)}>Guardar</button>
-              </>)
-              : (
+              </>
+            ) : (
               <>
                 <span> {index + 1}</span>
                 <span className={style['service-name']}> {service.name}</span>
-                <button className={style['edit-button']} onClick={() => handleEditClick(service.id)}>View</button>
-                <button className={style['delete-button']} onClick={() => handleDeleteClick(service.id)}>Delete</button>
+                <button className={style['edit-button']} onClick={() => handleEditClick(service.id)}>
+                  View
+                </button>
+                <button className={style['delete-button']} onClick={() => handleDeleteClick(service.id)}>
+                  Delete
+                </button>
               </>
-                )}
+            )}
           </li>
         ))}
       </ul>
