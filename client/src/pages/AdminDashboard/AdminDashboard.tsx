ed3f413a35df513d26d47196cb000e6b63efe194
GCespedes <giovannice22@hotmail.com>
2023-09-12 09:10:43 -0500

    fix: :bug: fix-some-typescript-bugs

--- client/src/pages/AdminDashboard/AdminDashboard.tsx
+++ client/src/pages/AdminDashboard/AdminDashboard.tsx
@@ -7,27 +7,29 @@ import style from './AdminDashboard.module.css'
 import { useParams } from 'react-router-dom'
 import { useSelector, useDispatch } from 'react-redux'
 import { getSellerbyId, getUsers } from '../../redux/actions'
+import type { RootState } from '../../redux/types'
+import type { ServiceProvider } from '../../interfaces'
 
 const AdminDashboard: React.FC = () => {
   const [activeItem, setActiveItem] = useState<string>('')
   const dispatch = useDispatch()
   const { id } = useParams()
-  const sellerdetail = useSelector((state) => state.sellerdetail) as ServiceProvider
-  const users = useSelector((state) => state.users)
+  const sellerdetail = useSelector((state: RootState) => state.sellerdetail) as ServiceProvider
+  const users = useSelector((state: RootState) => state.users)
 
   useEffect(() => {
     dispatch(getSellerbyId(id))
-    dispatch(getUsers())}
-  , [])
+    dispatch(getUsers())
+  }, [])
 
   return (
     <div>
       <SideBar setActiveItem={setActiveItem} activeItem={activeItem}/>
       <div className={style['right-section']}>
       {activeItem === 'Create' && <FormSeller />}
-      {activeItem === 'List' | activeItem === 'Services' && <SellerServices sellerName={sellerdetail.sellerName}
+      {(activeItem === 'List' || activeItem === 'Services') && <SellerServices sellerName={sellerdetail.sellerName}
       services={sellerdetail.servicesArray} setActiveItem={setActiveItem}/>}
-      {activeItem === 'Clients' && <Clients services={users} />}
+      {activeItem === 'Clients' && <Clients sellerName='Hola' services={users} />}
       </div>
     </div>
   )
