08ae4310faae554613cde4befad7755bea8a6c1b
GCespedes <giovannice22@hotmail.com>
2023-09-13 02:04:50 -0500

    build: :art: navbar-rework-finished

--- client/src/pages/AdminDashboard/AdminParts/ServiceList.tsx
+++ client/src/pages/AdminDashboard/AdminParts/ServiceList.tsx
@@ -5,6 +5,8 @@ interface Service {
   id: number
   name: string
   price: number
+  description: string
+  serviceCategories: string
 }
 
 interface Props {
@@ -14,15 +16,40 @@ interface Props {
 }
 
 const SellerServices: React.FC<Props> = ({ sellerName, services, setActiveItem }) => {
-  const [editedServiceId, setEditedServiceId] = useState<number | null>(null)
-  console.log(services)
+  const [ServiceId, setServiceId] = useState<string | null>(null)
+  const [Service, setService] = useState<Service | null>(null)
 
-  const handleEditClick = (id: number): void => {
-    setEditedServiceId(id)
+  const handleEditClick = (id: any): void => {
+    const serviceToEdit = services.find((service) => service.name === id)
+    if (serviceToEdit !== null) {
+      setServiceId(id)
+      setService(serviceToEdit)
+    }
   }
 
-  const handleDeleteClick = (id: number): void => {
+  const handleSaveClick = (): void => {
+    if (Service !== null) {
+      console.log('Guardar cambios:', Service)
+    }
+    setServiceId(null)
+    setService(null)
+  }
+
+  const handleCancelClick = (): void => {
+    setServiceId(null)
+    setService(null)
+  }
+
+  const handleDeleteClick = (id: any): void => {
     console.log(`Eliminar servicio con ID: ${id}`)
+  }
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>): void => {
+    const { name, value } = e.target
+    setService((prevService) => ({
+      ...prevService,
+      [name]: name === 'price' ? parseFloat(value) : value
+    }) as Service)
   }
 
   return (
@@ -33,25 +60,29 @@ const SellerServices: React.FC<Props> = ({ sellerName, services, setActiveItem }
       </div>
       <ul className={style['service-list']}>
         {services.map((service, index) => (
-          <li key={service.id} className={style['service-item']}>
-            {editedServiceId === service.id
+          <li key={service.name} className={style['service-item']}>
+            {ServiceId === service.name
               ? (<>
-                <input
-                  type="text"
-                  value={service.name}
-                  onChange={(e) => {
-                    console.log(`Editar servicio con ID ${service.id}: ${e.target.value}`)
-                  }}
-                />
-                <button onClick={() => { setEditedServiceId(null) }}>Guardar</button>
+              <span> {index + 1}</span>
+              <div>
+                <input type="text" name="name" value={Service?.name} onChange={handleChange}/>
+                <input type="text" name="description" value={Service?.description} onChange={handleChange}/>
+                <input type="text" name="serviceCategories" value={Service?.serviceCategories} onChange={handleChange}/>
+              </div>
+                <input type="text" name="price" value={Service?.price.toString()} onChange={handleChange}/>
+                <button onClick={handleSaveClick}>Guardar</button>
+                <button onClick={handleCancelClick}>Cancelar</button>
               </>)
               : (
               <>
                 <span> {index + 1}</span>
-                <span className={style['service-name']}> {service.name}</span>
+                <div>
+                  <span className={style['service-name']}> {service.name}</span>
+                  <span className={style['service-description']}> {service.description}</span>
+                </div>
                 <span className={style['service-price']}> $ {service.price}</span>
-                <button className={style['edit-button']} onClick={() => { handleEditClick(service.id) }}>Editar</button>
-                <button className={style['delete-button']} onClick={() => { handleDeleteClick(service.id) }}>Eliminar</button>
+                <button className={style['edit-button']} onClick={() => { handleEditClick(service.name) }}>Editar</button>
+                <button className={style['delete-button']} onClick={() => { handleDeleteClick(service.name) }}>Eliminar</button>
               </>
                 )}
           </li>
