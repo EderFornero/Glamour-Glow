4a78d0d725fba701b91eb1657e67ad396558787f
GCespedes <giovannice22@hotmail.com>
2023-09-15 08:38:08 -0500

    build: :art: filters-reword-and-css-update

--- client/src/components/FilterAndOrderCard/FilterAndOrderCard.tsx
+++ client/src/components/FilterAndOrderCard/FilterAndOrderCard.tsx
@@ -5,12 +5,11 @@ import FilterElement from './FilterElement/FilterElement'
 import type { CardsProps } from '../../interfaces/props'
 // components
 import OrderElement from './OrderElement/OrderElement'
-const FilterAndOrderCard: React.FC<CardsProps> = ({ allServices }: CardsProps) => {
-  console.log(allServices)
 
+const FilterAndOrderCard: React.FC<CardsProps> = ({ allServices, setSortOption }: CardsProps) => {
   return (
     <div className={style['div-container-order-filter']}>
-      <OrderElement />
+      <OrderElement setSortOption={setSortOption} />
       <FilterElement allServices={allServices} />
     </div>
   )
