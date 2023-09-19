502720f84ca2160eb113c641c317b275af913cd6
Lucas Cubile <cubilelucas@gmail.com>
2023-09-19 10:30:14 -0300

    build: :construction_worker: reports-working

--- client/src/components/Report/Report.tsx
+++ client/src/components/Report/Report.tsx
@@ -11,36 +11,41 @@ interface ReportProps {
 }
 
 const Report: React.FC<ReportProps> = ({ isOpen, onClose, id, route }) => {
-  const [report, setReport] = useState<string>('')
+  const [description, setDescription] = useState<string>('')
 
   const handleReportSubmit = async (event: React.FormEvent<HTMLFormElement>): Promise<void> => {
     event?.preventDefault()
-    const endpoint = `${API_URL}${route}/reports`
-    await axios.post(endpoint, { id, report })
-    setReport('')
+    try {
+      const endpoint = `${API_URL}${route}/reports`
+      await axios.post(endpoint, { id, description })
+      console.log('REPORT GENERADO')
+      setDescription('')
+    } catch (error) {
+      console.log(error)
+    }
   }
 
   return (
     <form className={`${styles['report-popup']} ${isOpen ? styles.open : ''}`} onSubmit={handleReportSubmit}>
       <label htmlFor='report' className={styles.label}>
-        Provide a description:{' '}
+        Provide a description:
       </label>
       <textarea
         name='report'
         placeholder='Enter your report here'
-        value={report}
+        value={description}
         onChange={(event) => {
-          setReport(event?.target.value)
+          setDescription(event?.target.value)
         }}
         className={styles.report}
         rows={6}
       ></textarea>
       <div className={styles.buttons}>
-        <button type='submit' className={styles.submit} disabled={report.length < 100}>
-          Submit
-        </button>
         <button type='button' className={styles.cancel} onClick={onClose}>
           Cancel
+        </button>
+        <button type='submit' className={styles.submit}>
+          Submit
         </button>
       </div>
     </form>
