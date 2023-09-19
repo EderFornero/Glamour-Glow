f098838b573590afd341269c2cb4d741bcecd2af
Lauty892 <lauta_lauta@hotmail.com>
2023-09-18 22:17:06 -0300

    build: :lipstick: style-and-new-metrics
    add style and new metrics components

--- client/src/components/Metrics/Charts/UsersChart.tsx
+++ client/src/components/Metrics/Charts/UsersChart.tsx
@@ -1,20 +1,17 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js'
 import { Pie } from 'react-chartjs-2'
-import { getUserMetrics } from '../../../redux/actions'
-import { useDispatch, useSelector } from 'react-redux'
-import type { RootState } from '../../../redux/types'
 
 ChartJS.register(ArcElement, Tooltip, Legend)
 
-const Pies = (): JSX.Element => {
-  const dispatch = useDispatch()
-  useEffect(() => {
-    dispatch(getUserMetrics())
-  }, [dispatch])
-  const metrics = useSelector((state: RootState) => state.userMetrics)
-  console.log(metrics)
+interface UsersPiesProps {
+  userMetrics: {
+    activeUsers: number
+    inactiveUsers: number
+  }
+}
 
+const Pies: React.FC<UsersPiesProps> = ({ userMetrics }): JSX.Element => {
   const options = {
     responsive: true,
     maintainAspectRatio: false
@@ -25,17 +22,17 @@ const Pies = (): JSX.Element => {
     datasets: [
       {
         label: 'Users Analytics',
-        data: [metrics.activeUsers, metrics.inactiveUsers],
+        data: [userMetrics.activeUsers, userMetrics.inactiveUsers],
         backgroundColor: [
-          'rgba(255, 99, 132, 0.2)',
-          'rgba(255, 206, 86, 0.2)',
+          'rgba(62, 56, 182, 0.2)',
+          'rgba(62, 56, 182, 1)',
           'rgba(54, 162, 235, 0.2)',
           'rgba(75, 192, 192, 0.2)',
           'rgba(153, 102, 255, 0.2)'
         ],
         borderColor: [
-          'rgba(255, 99, 132, 1)',
-          'rgba(255, 206, 86, 1)',
+          'rgba(62, 56, 182, 1)',
+          'rgba(0, 0, 0, 1)',
           'rgba(54, 162, 235, 1)',
           'rgba(75, 192, 192, 1)',
           'rgba(153, 102, 255, 1)'
--- client/src/components/Metrics/Metrics.tsx
+++ client/src/components/Metrics/Metrics.tsx
@@ -1,17 +1,36 @@
-import React from 'react'
-import Pies from './Charts/UsersChart'
+import React, { useEffect } from 'react'
+import UsersPies from './Charts/UsersChart'
 import SellersPies from './Charts/SellersChart'
+import style from './Metrics.module.css'
+import SellerUsersPie from './Charts/SellersUsersChart'
+import AllAccountPies from './Charts/AllAccountsChart'
+import { getUserMetrics, getSellerMetrics } from '../../redux/actions'
+import { useDispatch, useSelector } from 'react-redux'
+import type { RootState } from '../../redux/types'
 
 const Metrics = (): JSX.Element => {
-  return (
-    <div>
-      <div>
-        <SellersPies />
-      </div>
-      <div>
-        <Pies />
-      </div>
+  const dispatch = useDispatch()
+  useEffect(() => {
+    dispatch(getUserMetrics())
+    dispatch(getSellerMetrics())
+  }, [dispatch])
+  const userMetrics = useSelector((state: RootState) => state.userMetrics)
+  const sellerMetrics = useSelector((state: RootState) => state.sellerMetrics)
 
+  return (
+    <div className={style.content}>
+      <div className={style.unit}>
+        <SellersPies sellerMetrics={sellerMetrics}/>
+      </div>
+      <div className={style.unit}>
+        <UsersPies userMetrics={userMetrics}/>
+      </div>
+      <div className={style.unit}>
+        <SellerUsersPie userMetrics={userMetrics} sellerMetrics={sellerMetrics}/>
+      </div>
+      <div className={style.unit}>
+        <AllAccountPies userMetrics={userMetrics} sellerMetrics={sellerMetrics}/>
+      </div>
     </div>
   )
 }
