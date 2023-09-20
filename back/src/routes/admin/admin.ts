114f3fbfe6955abe65b0cd0a9fdd7114e6f30b80
Sebaa <sebaamdq96@gmail.com>
2023-09-20 10:59:26 -0300

    build: :bug: fix-bug-routes-admin

--- back/src/routes/admin/admin.ts
+++ back/src/routes/admin/admin.ts
@@ -1,50 +1,117 @@
-import { Router } from 'express'
-import { schemaValidation } from '../../middlewares/schemaValidator.middleware'
-import { rolePermissions } from '../../middlewares/authorization.middleware'
-import passport from 'passport'
-import { readAndDeleteUserSchema } from '../../schemas/userSchema'
-import { readAndDeleteSellerSchema } from '../../schemas/sellerSchema'
+import { Router } from "express";
+import { schemaValidation } from "../../middlewares/schemaValidator.middleware";
+import { rolePermissions } from "../../middlewares/authorization.middleware";
+import passport from "passport";
+import { readAndDeleteUserSchema, readAndReActiveUserSchema } from "../../schemas/userSchema";
+import {
+  readAndActiveSellerSchema,
+  readAndDeleteSellerSchema,
+} from "../../schemas/sellerSchema";
 import {
   deleteReviewsAdminController,
   deleteServiceAdminController,
   readSellersMetricsController,
   readUsersMetricsController,
   getPagesVisitsController,
+  getPaymentsController,
+} from "../../controllers/admin";
+import { deleteReviewSchema } from "../../schemas/reviewSchema";
+import { readAndDeleteServiceSchema } from "../../schemas/serviceSchema";
+import { deleteReportSchema } from "../../schemas/reportSchema";
+import { deleteReport, getReports } from "../../controllers/reports";
+import { transferPayment } from "../../controllers/admin/transferPayment";
+import { disableSeller } from "../../controllers/sellers";
+import { disableUser } from "../../controllers";
+import { enableUser } from "../../controllers/users/reActiveUser";
+import { enableSeller } from "../../controllers/sellers/reActiveSeller";
+
+const router = Router();
+
+router.get(
+  "/payments",
+  passport.authenticate("jwt", { session: false }),
+  rolePermissions("admin"),
   getPaymentsController
-} from '../../controllers/admin'
-import { deleteReviewSchema } from '../../schemas/reviewSchema'
-import { readAndDeleteServiceSchema } from '../../schemas/serviceSchema'
-import { deleteReportSchema } from '../../schemas/reportSchema'
-import { deleteReport, getReports } from '../../controllers/reports'
-import { transferPayment } from '../../controllers/admin/transferPayment'
-import { disableSellerController } from '../../controllers/sellers'
-import { disableUser } from '../../controllers'
-
-const router = Router()
-
-router.get('/payments', passport.authenticate('jwt', { session: false }), rolePermissions('admin'), getPaymentsController)
-
-router.get('/userMetrics', passport.authenticate('jwt', { session: false }), rolePermissions('admin'), readUsersMetricsController)
-
-router.get('/sellerMetrics', passport.authenticate('jwt', { session: false }), rolePermissions('admin'), readSellersMetricsController)
-router.get('/visits', passport.authenticate('jwt', { session: false }), rolePermissions('admin'), getPagesVisitsController)
-
+);
+router.get(
+  "/userMetrics",
+  passport.authenticate("jwt", { session: false }),
+  rolePermissions("admin"),
+  readUsersMetricsController
+);
+router.get(
+  "/sellerMetrics",
+  passport.authenticate("jwt", { session: false }),
+  rolePermissions("admin"),
+  readSellersMetricsController
+);
+router.get(
+  "/visits",
+  passport.authenticate("jwt", { session: false }),
+  rolePermissions("admin"),
+  getPagesVisitsController
+);
 router.put(
-  "/disable/:id",
+  "/user/disable/:id",
   passport.authenticate("jwt", { session: false }),
   rolePermissions("admin"),
   schemaValidation(readAndDeleteUserSchema),
   disableUser
 );
-router.put('/disable/:id', passport.authenticate('jwt', { session: false }), schemaValidation(readAndDeleteSellerSchema), disableSellerController)
+router.put(
+  "/seller/disable/:id",
+  passport.authenticate("jwt", { session: false }),
+  schemaValidation(readAndDeleteSellerSchema),
+  rolePermissions("admin"),
+  disableSeller
+);
+router.put(
+  "/seller/enable/:id",
+  passport.authenticate("jwt", { session: false }),
+  schemaValidation(readAndActiveSellerSchema),
+  rolePermissions("admin"),
+  enableSeller
+);
+router.put(
+  "/user/enable/:id",
+  passport.authenticate("jwt", { session: false }),
+  schemaValidation(readAndReActiveUserSchema),
+  rolePermissions("admin"),
+  enableUser
+);
 
-router.delete('/dropReview/:id', passport.authenticate('jwt', { session: false }), rolePermissions('admin'), schemaValidation(deleteReviewSchema), deleteReviewsAdminController)
+router.delete(
+  "/dropReview/:id",
+  passport.authenticate("jwt", { session: false }),
+  rolePermissions("admin"),
+  schemaValidation(deleteReviewSchema),
+  deleteReviewsAdminController
+);
+router.delete(
+  "/dropService/:id",
+  passport.authenticate("jwt", { session: false }),
+  rolePermissions("admin"),
+  schemaValidation(readAndDeleteServiceSchema),
+  deleteServiceAdminController
+);
+router.get(
+  "/reports",
+  passport.authenticate("jwt", { session: false }),
+  rolePermissions("admin"),
+  getReports
+);
+router.post(
+  "/payment/:id",
+  passport.authenticate("jwt", { session: false }),
+  rolePermissions("admin"),
+  transferPayment
+);
+router.delete(
+  "/reports/:id",
+  passport.authenticate("jwt", { session: false }),
+  rolePermissions("admin"),
+  schemaValidation(deleteReportSchema),
+  deleteReport
+);
 
-router.delete('/dropService/:id', passport.authenticate('jwt', { session: false }), rolePermissions('admin'), schemaValidation(readAndDeleteServiceSchema), deleteServiceAdminController)
-
-router.get('/reports', passport.authenticate('jwt', { session: false }), rolePermissions('admin'), getReports)
-router.post('/payment/:id', passport.authenticate('jwt', { session: false }), rolePermissions('admin'), transferPayment)
-
-router.delete('/reports/:id', passport.authenticate('jwt', { session: false }), rolePermissions('admin'), schemaValidation(deleteReportSchema), deleteReport)
-
-export default router
+export default router;
